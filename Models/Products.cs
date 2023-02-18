8c35399048fd5931daf2186c4cfa66ba92193add
Austin Fogle <A-Fogle1994@outlook.com>
2023-02-18 18:16:20 -0500

    First Commit for Store-API, Created Products Model.

---
+++ Models/Products.cs
@@ -0,0 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Store_API.Models;
+
+public class Poducts 
+{
+    public int ProductId { get; set; }
+
+    [Required]
+    public string? Title { get; set; }
+
+    [Required]
+    public double Price { get; set; }
+
+    [Required]
+    public string? Description { get; set; }
+    
+    [Required]
+    public string? Picture { get; set; }
+}
